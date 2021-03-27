d18636a0105d7b6b913ba35f2a6d9e14c6d38d71
Abdulaziz <abdulazizfaactory@gmail.com>
2021-03-27 21:30:12 +0500

    EX-1: Done

--- webpage/index.php
+++ webpage/index.php
@@ -1,31 +1,49 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<title>Validating Forms</title>
-		<link href="style.css" type="text/css" rel="stylesheet" />
-	</head>
-	
-	<body>
-		<h1>Registration Form</h1>
+<head>
+    <title>Validating Forms</title>
+    <link href="style.css" type="text/css" rel="stylesheet"/>
+</head>
 
-		<p>
-			This form validates user input and then displays "Thank You" page.
-		</p>
-		
-		<hr />
-		
-		<h2>Please, fill below fields correctly</h2>
-		<dl>
-			<dt>Name</dt>
-			<dd>
-				???
-			</dd>
-			
-			<!-- Write other fiels similar to Name as specified in lab6.pdf -->
-		</dl>
-		
-		<div>
-			Register
-		</div>
-	</body>
+<body>
+<?php
+$name = "";
+$isNameValid = "";
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $isNameValid = preg_match("/^\D{2,}$/", $name);
+    $email = $_POST["email"];
+    $isEmailValid = preg_match("/^\D{2,}$/", $name);
+
+}
+?>
+<h1>Registration Form</h1>
+
+<p>
+    This form validates user input and then displays "Thank You" page.
+</p>
+
+<hr/>
+
+<h2>Please, fill below fields correctly</h2>
+<form action="index.php" method="post">
+    <dl>
+        <dt>Name</dt>
+        <dd>
+            <input type="text" name="name" class="<?= $isNameValid ? "" : "invalid" ?>" value="<?= $name ?>" required>
+        </dd>
+
+        <dt>Email</dt>
+        <dd>
+            <input type="text" name="email" class="<?= $isEmailValid ? "" : "invalid" ?>" value="<?= $email ?>" required>
+        </dd>
+
+        <dt>&nbsp;</dt>
+        <dd><input type="submit" value="submit"></dd>
+    </dl>
+
+</form>
+</body>
 </html>
